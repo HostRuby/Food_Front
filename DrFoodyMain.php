242f43a1f0ab07b8b3dbdb1b2ab78da7f1579bc6
HostRuby <146001587+HostRuby@users.noreply.github.com>
2023-10-30 22:06:24 +0900

    Add files via upload

---
+++ DrFoodyMain.php
@@ -0,0 +1,18 @@
+<script>#!/usr/bin/env php</script>
+<html>
+	<head><title> Dr.Foody </title></head>
+	<body><center><br><h2> Dr.Foody </h2><hr><br>
+		<form name=f1 method=post action="#">
+		<table width=400 border=0 bordercolor=#000
+			cellspacing=1 height=180 bgcolor=#FFC>
+			<tr><td width=5%></td>
+			<td> <br><b>&nbsp;&nbsp; Dr.Foody는 만성질환 환자에게<br><br> 식단을 추천하는 앱입니다 </b><br><br>
+			</td><td width=5%></td>
+			</tr>
+		</table>
+		버튼을 통해 테이블 부르기
+		<input type="submit" value=" ◀ 테이블 생성하기 ▶ ">
+		</form>
+		</center>
+	</body>
+</html>
